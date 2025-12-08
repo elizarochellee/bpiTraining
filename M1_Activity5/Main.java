979121d4ed6dae8544dda4da258ba5b12eeab756
Eliza Rochelle Gatbonton <ergatbonton@headoffice.corp.bpi.com.ph>
2025-12-08 21:08:05 +0800

    M1_Activity5

---
+++ M1_Activity5/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package ph.com.bpi.M1Activity5;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 50; i++) {
+            sum += i;
+        }
+
+        System.out.println("The sum of numbers from 1 to 50 is: " + sum);
+	}
+
+}
